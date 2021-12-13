d135050f4a8acc01dc796d7aaf2ee31e880f2295
jelsync <jelsyn@gmail.com>
2021-12-12 22:42:43 -0600

    Objetos literales

---
+++ 01-reforzamiento/src/typesScript/ObjetosLiterales.tsx
@@ -0,0 +1,29 @@
+interface Persona {
+  nombre: String;
+  edad: String;
+  direccion: Direccion;
+}
+
+interface Direccion {
+  ciudad: String;
+  municipio: String;
+}
+
+export const ObjetosLiterales = () => {
+  const persona: Persona = {
+    nombre: "jelsyn",
+    direccion: {
+      ciudad: "Tegucigalpa",
+      municipio: "Disitrito central",
+    },
+  };
+
+  return (
+    <>
+      <h3>Objetos literales</h3>
+      <code>
+        <pre>{JSON.stringify(persona, null, 2)}</pre>
+      </code>
+    </>
+  );
+};
