a564285704947881a9bd4c426b334c2893b6ee37
jelsync <jelsyn@gmail.com>
2021-12-12 22:40:50 -0600

    Inicializando reposirorio npx create-react-app my-app --template typescript

---
+++ 01-reforzamiento/src/App.tsx
@@ -0,0 +1,11 @@
+import { ObjetosLiterales } from "./typesScript/ObjetosLiterales";
+
+const App = () => {
+  return <div className="mt-2">
+    <h1>Intro a TypeScript</h1>
+    <hr/>
+    <ObjetosLiterales/>
+  </div>;
+};
+
+export default App;
